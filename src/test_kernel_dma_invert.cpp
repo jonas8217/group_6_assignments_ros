1f58c293610acd75f9593aca230c498163761045
Frank-JJ <frankjuuljensen1@gmail.com>
2023-11-06 03:38:43 +0100

    Look half below and half above range

--- src/test_kernel_dma_invert.cpp
+++ src/test_kernel_dma_invert.cpp
@@ -255,11 +255,11 @@ int main()
 	printf("\n\n");
 	
 	//pmem.gather(inp_buff, TX_OFFSET, LENGTH_INPUT);
-	uint32_t *io_buff = (uint32_t *)malloc(LENGTH);
+	uint32_t *io_buff = (uint32_t *)malloc(LENGTH*2);
 	
-	pmem.gather(io_buff, RX_OFFSET, LENGTH);
+	pmem.gather(io_buff, RX_OFFSET-LENGTH/2, LENGTH*2);
 	
-	for (int i = 0; i < LENGTH / sizeof(uint32_t); i++) {
+	for (int i = 0; i < (LENGTH*2) / sizeof(uint32_t); i++) {
 		printf("\nio_buff: %i %x\n\r", i, io_buff[i]);
 	}
 	
